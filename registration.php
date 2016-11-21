d6b80a42a3fb999bcca410c64775cdc5d24123d9
Pascal <pascaladriaansen@gmail.com>
2016-11-21 12:28:36 +0100

    Remove unnecessary stuff, change theme name, inherit from luma

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/LAB51/boilerplate',
+    'frontend/LAB51/test',
     __DIR__
 );
